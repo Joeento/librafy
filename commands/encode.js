dec99a846afe263eccb53a142fc2b636d5552fe9
Eric Kudler <joeento@gmail.com>
2020-07-21 14:19:28 -0400

    Adds command for generating a jwt despite no login endpoint

---
+++ commands/encode.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const User = require('../models/User');
+
+async function main(id) {
+    mongoose.connect(config.mongo_url, { useUnifiedTopology: true, useNewUrlParser: true });
+    const user = await User.findById(id);
+
+
+    console.log(jwt.sign(user.toJSON(), config.token_secret));
+    mongoose.connection.close();
+}
+
+main(process.argv[2]);
